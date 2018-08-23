10de3f0652f98f6ce4481ed7e57ba61c7e49d899
Quavo <mathiastb13@gmail.com>
2018-08-24 01:22:41 +0200

    Updated Regex for detecting links

--- window.js
+++ window.js
@@ -35,19 +35,21 @@ function winHandle(query) {
 function isEmpty(content) { return !content.replace(/^\s+/g, ''); }
 
 function isURL(str) {
-    var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
-      '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
-      '((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
-      '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
-      '(\?[;&a-z\d%_.~+=-]*)?'+ // query string
-      '(\#[-a-z\d_]*)?$','i'); // fragment locater
-    if(!pattern.test(str)) {
-      alert("Please enter a valid URL.");
-      return false;
+    var pattern = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+    if (pattern.test(str)) {
+        return true;
     } else {
-      return true;
+        return false;
     }
-  }
+}
+
+function isValidYoutubeURL(url) {
+    var p = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+    if(url.match(p)){
+        return url.match(p)[1];
+    }
+    return false;
+}
 
 function handleEvent(event) {
     if (event === "setupLinkParser") {
@@ -58,7 +60,7 @@ function handleEvent(event) {
             event.preventDefault();
             if (event.keyCode === 13) {
                 if (linkbox.value === "" || isEmpty(linkbox.value)) return;
-                if (!isURL()) return;
+                if (!isURL(linkbox.value) || !isValidYoutubeURL(linkbox.value)) return;
 
                 ipcRenderer.send("message", "Link: " + linkbox.value);
                 linkbox.value = "";
