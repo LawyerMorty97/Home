26b2703a1311f184a10672042859d06d69f5f5a1
Mathias Berntsen <mathiastb13@gmail.com>
2018-10-17 13:09:53 +0200

    Updated Electron

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ if (!Date.now) {
     }
 }
 
-var url = "https://www.youtube.com/watch?v=01FYyb81Cdg";
+var url = "https://www.youtube.com/watch?v=kNUKLMkcVEU";
 var options = {
     quality: "highest",
     filter: "audioandvideo",
@@ -18,21 +18,23 @@ var options = {
 //var fileName = Date.now + "-vid";
 //ytdl(url, options).pipe(fs.createWriteStream("downloads/" + fileName + ".mp4"));
 
+var dlFolder = "downloads2/";
+
 function init() {
     // Check if a downloads folder exists
-    if (!fs.existsSync("downloads/")) {
-        fs.mkdirSync("downloads/");
+    if (!fs.existsSync(dlFolder)) {
+        fs.mkdirSync(dlFolder);
     }
 }
 
 function startDownload(ytURL, title) {
-    if (fs.existsSync("downloads/" + title + ".mp4")) {
+    if (fs.existsSync(dlFolder + title + ".mp4")) {
         console.log("Video has already been downloaded. Goodbye.");
         return;
     }
 
     var video = ytdl(ytURL, options);
-    video.pipe(fs.createWriteStream("downloads/" + title + ".mp4"));
+    video.pipe(fs.createWriteStream(dlFolder + title + ".mp4"));
     video.on('response', (res) => {
         var totalSize = res.headers['content-length'];
         var dataRead = 0;
@@ -65,7 +67,7 @@ function run() {
         var title = info.title;
         //console.log(info);
         console.log("Downloading '" + title + "' by " + author);
-        startDownload(url, title);
+        startDownload(url, Date.now());
     });
 }
 
